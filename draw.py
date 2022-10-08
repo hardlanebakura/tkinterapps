3f2b022f0aa58aa4c9d11f0179966e46ddfbf240
= <hardlane2000@gmail.com>
2022-10-08 08:04:41 +0200

    committing before upload

--- draw.py
+++
@@ -1,9 +0,0 @@
-from PIL import Image, ImageDraw
-def line(output_path):
-    image = Image.new("RGB", (400, 400), "red")
-    points = [(100, 100), (150, 200), (200, 50), (400, 400)]
-    draw = ImageDraw.Draw(image)
-    draw.line(points, width=15, fill="green", joint="curve")
-    image.save(output_path)
-if __name__ == "__main__":
-    line("draw_test.jpg")
